27dd40098b11eb43b9fe08da6f04d0a00d1676a0
caohanyang <caohanyang007@gmail.com>
2017-09-28 15:15:41 +0200

    be able to generate candidate actions

--- database.js
+++ database.js
@@ -3,6 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 var winston = require('winston');
 var baseId = null;
+var asyncLoop = require('node-async-loop');
 
 function write_base_scenario(dbUrl, scenario_base, callback) {
 	winston.info(`Save base scenario in ${dbUrl}`);
@@ -38,14 +39,14 @@ function write_base_scenario(dbUrl, scenario_base, callback) {
 	});
 }
 
-function write_base_action(dbUrl, action) {
+function write_base_action(dbUrl, action, callback) {
 	winston.info(`Save base action in ${dbUrl}`);
 	MongoClient.connect(dbUrl)
 	.then(db => {
 		db.collection('action', (err, actionCollection) => {
 			if (err) {
 				winston.error(err);
-				db.close();
+				// db.close();
 			} else {
 
 				// create new action item
@@ -54,26 +55,27 @@ function write_base_action(dbUrl, action) {
 
 				// use findOneAndReplace to save unique action in action
 				actionCollection.findOneAndReplace({'action':action},actionItem,{upsert:true})
-					.then(()=> {
+					.then((actionOne)=> {
 						winston.info("Success to save base action");
+						if (actionOne.value !== null) {
+							callback(actionOne.value._id);
+						} else {
+							callback(actionOne.lastErrorObject.upserted)
+						}
+
+						db.close();
 					}).catch(err => {
 						winston.error(err);
 					})
 
 			}
 		});
-
-        db.close();
-
+        
 	}).catch(err => {
 		winston.info(err);
 	});
 }
 
-function existAction(db, value) {
-
-	
-};
 
 function write_noise_scenario(dbUrl, scenario_noise, cid, flag) {
 	winston.info(`Save noise scenario in ${dbUrl}`);
@@ -109,39 +111,93 @@ function write_noise_scenario(dbUrl, scenario_noise, cid, flag) {
 
 
 
-function write_candidate_action(dbUrl, baseId, index, scenario) {
-	winston.info(`Save candidate action in ${dbUrl}`);
+function write_candidate_action(dbUrl, baseId, index, can_set) {
+	winston.info(`Save candidate action in ${dbUrl}, ${baseId}, ${index}, ${can_set.actions.length}`);
+
 	MongoClient.connect(dbUrl)
 	.then(db => {
-		db.collection('candidate', (err, candidateCollection) => {
+		db.collection('action', (err, actionCollection) => {
 			if (err) {
 				winston.error(err);
-				db.close();
+			    db.close();
 			} else {
-                //save every candidate action into candidate
-                for (var i = 0; i < scenario.actions.length; i++) {
-                    var can_action = {};
-                    can_action._id = ObjectID();
-                    can_action.bid = baseId;
-                    can_action.aid = index;
-                    can_action.action = scenario.actions[i];
 
-                    candidateCollection.save(can_action)
-                        .then(()=> {
-                            winston.info("Success to save candidate action");
-                        }).catch(err => {
-                            winston.error(err);
-                        });
-                }
-				
+				// var tests = [];
+				for (var i = 0; i < can_set.actions.length; ++i) {
+
+					// use findOneAndReplace to save unique action in action
+					actionCollection.findOneAndReplace(
+						{"action": can_set.actions[i]},
+						{"action": can_set.actions[i]},
+						{upsert : true, returnOriginal: false}
+						
+				    )
+					.then((actionOne)=> {
+						winston.info("Success to save candidate into action table");
+
+						// create new action item
+						var actionItem = null;
+						var aid = null;		
+
+						if (actionOne.value !== null) {
+							// exist one
+							aid = actionOne.value._id;
+							actionItem = actionOne.value.action;
+						} else {
+							// add new one
+							aid = actionOne.lastErrorObject.upserted;
+							actionItem = actionOne.value.action;
+						}
+
+						MongoClient.connect(dbUrl)
+						.then(db => {
+							db.collection('candidate', (err, candidateCollection) => {
+								if (err) {
+									winston.error(err);
+									db.close();
+								} else {
+
+									//save every candidate action into candidate
+										var can_action = {};
+										can_action._id = ObjectID();
+										can_action.bid = baseId;
+										can_action.abid = index;
+										can_action.aid = aid;
+										can_action.action = actionItem;
+										
+										// console.log(can_action);
+										candidateCollection.save(can_action)
+											.then(()=> {
+												winston.info("Success to save candidate action");
+											}).catch(err => {
+												winston.error(err);
+											});
+		
+								}
+							});
+					
+							db.close();
+					
+						}).catch(err => {
+							winston.info(err);
+						});
+
+
+					}).catch(err => {
+						winston.error(err);
+					})
+
+					
+				}
+							
+				db.close();
 			}
 		});
-
-        db.close();
-
+        
 	}).catch(err => {
 		winston.info(err);
 	});
+
 }
 
 
--- gen_candi.js
+++ gen_candi.js
@@ -6,6 +6,8 @@ var crawl_action = require('./crawl_action.js');
 var fs = require('fs');
 const argv = require('yargs').argv;
 var database = require('./database.js');
+var asyncLoop = require('node-async-loop');
+var winston = require('winston');
 
 const serverNames = {
 	mongoServerName : argv.mongo
@@ -18,27 +20,63 @@ const dbUrl = `mongodb://${serverNames.mongoServerName}:27017/wat_storage`;
 database.write_base_scenario(dbUrl, scenario_base, (baseId) => {
 
 	console.log(baseId);
+
 	// 2. generate action for each base action
-    for (var i = 0; i < scenario_base.actions.length; ++i) {
-		database.write_base_action(dbUrl, scenario_base.actions[i]);
-	}
+	asyncLoop(scenario_base.actions, function(action, next)
+	{
+
+		database.write_base_action(dbUrl, action, (aid) => {
+			console.log(aid);
+			next();
+	    });
+	
+	}, function(err) {
+		if (err) {
+			console.log('Error: ' + err.message);
+			return;
+		}
+	
+		console.log('Finished');
+	});
+
+
+
 
 	// 3. generate candidate for each action
-	// var tests = [];
-	// for (var i = 0; i < scenario_base.actions.length; ++i) {
-	// 	tests.push(gen_candi_actions(baseId, scenario_base.actions[i], i));
-	// }
+	var tests = [];
+	for (var i = 0; i < scenario_base.actions.length; ++i) {
+		tests.push(gen_candi_actions(baseId, i));
+	}
 
-	// Promise.all(tests).then(function() {
-	// 	console.log("all the tests were executed");
+	Promise.all(tests).then(function() {
+		console.log("all the tests were executed");
+	});
+
+	// var j = 0;
+	// asyncLoop(scenario_base.actions, function(action, next)
+	// {
+
+	// 	gen_candi_actions(baseId, j, ()=> {
+	// 		j++;
+	// 		next();
+	// 	})
+       
+	// }, function(err) {
+	// 	if (err) {
+	// 		console.log('Error: ' + err.message);
+	// 		return;
+	// 	}
+	
+	// 	console.log('Finished');
 	// });
+
 
 });
 
 
 
 
-function gen_candi_actions(baseId, item, index) {
+function gen_candi_actions(baseId, index) {
 	
 	var nightmare = new Nightmare({show:true});				
 	var scenario = new wat_action.Scenario(scenario_str);
